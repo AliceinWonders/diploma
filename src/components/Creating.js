120c5432a6111140b07bbae359175d4ee4a6de7f
AliceInWonders <zubatovaa@mail.ru>
2023-04-25 17:32:10 +0300

    modified:   src/index.css

---
+++ src/components/Creating.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header"
+import { Route, Routes } from "react-router-dom";
+
+function Creating(props) {
+
+    return(
+        <div className ="Creating" >
+        <Header />
+        
+        </div>
+    )
+}
