72faf2f7f9d4949bb2ea91d2aef7526aa68e18d8
AliceInWonders <zubatovaa@mail.ru>
2023-04-28 11:17:23 +0300

    modified:   src/index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,38 @@
 import { Link, NavLink } from 'react-router-dom';
 import React, { useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+  crossorigin="anonymous"
+/>
 
 function Header(props){
-  
-/*   const callback = () => {
-    <NavLink to="/creating"><button><p>Новая запись</p></button></NavLink>
-       <NavLink to="/profile"><button><p>Профиль</p></button></NavLink></>
-      <NavLink to="/main"><button><p>Выйти</p></button></NavLink>
-    };    
-  const button = document.querySelector('#menu');
-  button.addEventListener('click', callback); */
-
+    
   return(
       
         <div className="Header">
             <nav>
-                <NavLink  to="/main"><button ><p><b>Travel App</b></p></button></NavLink>
-
-                <button id = "menu"><img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png"></img></button>
+                <NavLink  to="/main"><button><p><b>Travel App</b></p></button></NavLink>
                 
+                {/* <button><img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png"></img></button> */}
+                
+                <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                <img src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png"></img>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <ul>
+                  <Dropdown.Item> <NavLink  to="/creating">Новая запись</NavLink></Dropdown.Item>
+                  <Dropdown.Item> <NavLink  to="/profile">Профиль</NavLink></Dropdown.Item>
+                  <Dropdown.Item> <NavLink  to="/main">Выйти</NavLink></Dropdown.Item>
+                  </ul>
+                </Dropdown.Menu>
+              </Dropdown>
+              
                 <div class="search-container">
                   <form action="../action_page.php">
                     <input type="text" placeholder="Поиск.." name="search"></input>
