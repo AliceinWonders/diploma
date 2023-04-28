c2becf9e7b0f1a761a9437c76df39bf9316493fb
AliceInWonders <zubatovaa@mail.ru>
2023-04-28 13:48:33 +0300

    modified:   src/components/Post.js

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "./Header"
 import { Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import Creating from "./Creating";
+import { Profile } from "./Profile";
 
 function MainPage() {
     
@@ -8,7 +11,10 @@ function MainPage() {
         <div className="MainPage">
             <Header />
             <Routes>
-                
+                <Route path="/" element={<MainPage />}/>
+                <Route path="/main" element={<MainPage />}/>
+                <Route path="/creating" element={<Creating />}/>
+                <Route path="/profile" element={<Profile />}/>
             </Routes>
          </div>)
                           
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,3 +2,7 @@ import React from "react";
 import Header from "./Header"
 import { Route, Routes } from "react-router-dom";
 
+<div className ="Profile" >
+        <Header />
+        
+        </div>
