fa09aaa5dafed214d57fbbd9b03a04d8863f493c
AliceinWonders <zubatovaa@mail.ru>
2023-05-15 01:00:51 +0300

    modified:   src/components/MainPage.js

--- src/components/Creating.js
+++ src/components/Creating.js
@@ -2,12 +2,3 @@ import React from "react";
 import Header from "./Header"
 import { Route, Routes } from "react-router-dom";
 
-function Creating(props) {
-
-    return(
-        <div className ="Creating" >
-        <Header />
-        
-        </div>
-    )
-}
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,21 +1,17 @@
 import React from "react";
 import Header from "./Header"
+import PostList from "./PostList";
 import { Route, Routes } from "react-router-dom";
-import Header from "./Header";
 import Creating from "./Creating";
 import { Profile } from "./Profile";
+import { DropdownButton } from "react-bootstrap";
 
 function MainPage() {
     
     return( 
         <div className="MainPage">
             <Header />
-            <Routes>
-                <Route path="/" element={<MainPage />}/>
-                <Route path="/main" element={<MainPage />}/>
-                <Route path="/creating" element={<Creating />}/>
-                <Route path="/profile" element={<Profile />}/>
-            </Routes>
+            <PostList/>
          </div>)
                           
 }
